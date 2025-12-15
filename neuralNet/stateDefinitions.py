1e30f32e226845d00602137ec64f349585f6005e
Matthew Reff <geniusammyralltheothernamesare@gmail.com>
2025-12-14 19:48:30 -0800

    Add first draft of Pytorch definitions
    add post linter values

---
+++ neuralNet/stateDefinitions.py
@@ -0,0 +1,60 @@
+from gymnasium.spaces import Dict, Sequence, Discrete, Text
+import string
+
+"""
+Orbs
+0 - Empty
+1 - Lightning
+2 - Frost
+3 - Dark
+4 - Plasma
+"""
+OrbSpace = Discrete(5)
+
+CardSpace = Text(min_length=1, max_length=100, charset=string.ascii_letters)
+PotionSpace = Text(min_length=1, max_length=100, charset=string.ascii_letters)
+ArtifactSpace = Text(min_length=1, max_length=100, charset=string.ascii_letters)
+BuffSpace = Text(min_length=1, max_length=100, charset=string.ascii_letters)
+IntentSpace = Dict(
+    {
+        "attack": Text(min_length=1, max_length=3, charset=string.digits),
+        "effect": Sequence(
+            Text(min_length=1, max_length=100, charset=string.ascii_letters)
+        ),
+    }
+)
+EnemySpace = Dict(
+    {
+        "name": Text(min_length=1, max_length=100, charset=string.ascii_letters),
+        "intent": Sequence(IntentSpace),
+        "health": Text(min_length=1, max_length=3, charset=string.digits),
+        "buffs": Sequence(BuffSpace),
+    }
+)
+
+ActionSpace = Dict(
+    {
+        "hand_cards": Sequence(CardSpace),
+        "potions": Sequence(PotionSpace),
+    }
+)
+
+StateSpace = Dict(
+    {
+        # Directly Interactive
+        "hand_cards": Sequence(CardSpace),
+        "potions": Sequence(PotionSpace),
+        # Personal State
+        "current_orbs": Sequence(OrbSpace),
+        "energy": Text(min_length=1, max_length=2, charset=string.digits),
+        "health": Text(min_length=1, max_length=3, charset=string.digits),
+        "artifacts": Sequence(ArtifactSpace),
+        "buffs": Sequence(BuffSpace),
+        # Additional Card
+        "discarded_cards": Sequence(CardSpace),
+        "exhausted_cards": Sequence(CardSpace),
+        "remaining_deck_cards": Sequence(CardSpace),
+        # Enemy
+        "enemies": Sequence(EnemySpace),
+    }
+)
